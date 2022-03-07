df4a98afea4f77496ad273ebe119d334073fea04
FakeFly <axelurizarmartin@gmail.com>
2022-03-07 19:01:14 +0000

    minorFix

--- src/components/pages/prestamos/ListadoPrestamos.js
+++ src/components/pages/prestamos/ListadoPrestamos.js
@@ -12,18 +12,16 @@ const ListadoPrestamos = () => {
             {loans.length > 0 && 
                 <ul className="mt-4 p-0">
                     {loans.map(loan => (
-                        <div>
-                            <li className="row mt-4 d-flex align-items-center justify-content-center" key={loan._id}>
-                                <h4 className="col-4 p-0 text-light">{loan.movieTitle}</h4>
-                                <div className="d-flex col-3 justify-content-start align-items-center p-0">
-                                    <p className="descripcionTitulo">Fecha Devolución:<span className="text-light ps-3">{loan.returnAt}</span></p>
-                                </div>
-                                <div className="d-flex col-3 justify-content-start align-items-center p-0">
-                                    <p className="descripcionTitulo">Estado:<span className="text-light ps-3">{loan.returned === false && 'No Devuelto'}{loan.returned === true && 'Devuelto'}</span></p>
-                                </div>
-                                <Link to={`/prestamos/${loan._id}`} className='p-0 col-2'><button className="btn btn-danger rounded-pill" type="submit">Detalles</button></Link>
-                            </li>
-                        </div>
+                        <li className="row mt-4 d-flex align-items-center justify-content-center" key={loan._id}>
+                            <h4 className="col-4 p-0 text-light">{loan.movieTitle}</h4>
+                            <div className="d-flex col-3 justify-content-start align-items-center p-0">
+                                <p className="descripcionTitulo">Fecha Devolución:<span className="text-light ps-3">{loan.returnAt}</span></p>
+                            </div>
+                            <div className="d-flex col-3 justify-content-start align-items-center p-0">
+                                <p className="descripcionTitulo">Estado:<span className="text-light ps-3">{loan.returned === false && 'No Devuelto'}{loan.returned === true && 'Devuelto'}</span></p>
+                            </div>
+                            <Link to={`/prestamos/${loan._id}`} className='p-0 col-2'><button className="btn btn-danger rounded-pill" type="submit">Detalles</button></Link>
+                        </li>
                     ))}
                 </ul>
             }    
